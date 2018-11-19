e8392510654955ce464befd9fdfa550a0aeef54f
GabyRobles <gabriela.soto.robles@hotmail.com>
2018-11-19 12:43:21 -0300

    PArte jimmy - servlet y valoracion bean modificado

---
+++ java/proyectoAura/src/java/control/ValoracionBean.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.ParameterMode;
+import javax.persistence.Persistence;
+import javax.persistence.StoredProcedureQuery;
+
+
+public class ValoracionBean {
+    //declarar la Api para manejar la persistencia de JPA
+    EntityManagerFactory factory = Persistence.createEntityManagerFactory("ProyectoAuraPU");
+    EntityManager em = factory.createEntityManager();
+    
+        //INSERTAR_VALORACION" (val in number,fch in date,comm in VARCHAR2,idof in number,idcons in number,idct in number,idsuc in number)
+    public void createValoracion(int valoracion, Date fecha,String comentario ,int idofert,int idconsumidor,int idcate,int idsucur ){
+        //SE CREA UNA VARIABLE DE PROCEDIMIENTO
+        StoredProcedureQuery storedProcedureQuery= em.createStoredProcedureQuery("OPERACIONES_CRUD.INSERTAR_VALORACION");
+        //variable del procedimiento en la bd
+        storedProcedureQuery.registerStoredProcedureParameter("val", Integer.class, ParameterMode.IN);
+        storedProcedureQuery.registerStoredProcedureParameter("fch", Date.class, ParameterMode.IN);
+        storedProcedureQuery.registerStoredProcedureParameter("comm", String.class, ParameterMode.IN);
+        storedProcedureQuery.registerStoredProcedureParameter("idof", Integer.class, ParameterMode.IN);
+        storedProcedureQuery.registerStoredProcedureParameter("idcons", Integer.class, ParameterMode.IN);
+        storedProcedureQuery.registerStoredProcedureParameter("idct", Integer.class, ParameterMode.IN);
+        storedProcedureQuery.registerStoredProcedureParameter("idsuc", Integer.class, ParameterMode.IN); 
+    
+        //ingreso de variables
+         storedProcedureQuery.setParameter("val", valoracion);
+         storedProcedureQuery.setParameter("fch", valoracion);
+         storedProcedureQuery.setParameter("comm", valoracion);
+         storedProcedureQuery.setParameter("idof", valoracion);
+         storedProcedureQuery.setParameter("idcons", valoracion);
+         storedProcedureQuery.setParameter("idct", valoracion);
+         storedProcedureQuery.setParameter("idsuc", valoracion);
+                 
+        //ejecutar procedimiento
+        storedProcedureQuery.execute(); 
+        
+    }
+    
+    
+}
